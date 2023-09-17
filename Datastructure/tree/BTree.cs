fd9bc6fee00bcc03cea74417fdb8d7bbd1cacc3f
Udhayarajan J <udhayarajan080@gmail.com>
2023-09-17 12:01:43 +0530

    [ refactor ] : B Tree functionalities are implemented and fix it

--- Datastructure/tree/BTree.cs
+++ Datastructure/tree/BTree.cs
@@ -8,10 +8,18 @@ namespace DataStructure_And_Algorithm.Datastructure.tree
 {
     public class BTreeNode
     {
-        public int n;
+        public int height;
         public int[] keys;
         public BTreeNode[] child;
         public bool leafs;
+
+        public int FindKey(int key)
+        {
+            for (int i = 0; i < this.height; i++)
+                if (this.keys[i] == key)
+                    return i;
+            return -1;
+        }
     }
     public class BTree
     {
@@ -30,7 +38,7 @@ namespace DataStructure_And_Algorithm.Datastructure.tree
             bTreeNode.keys = new int[2 * degree - 1];
             bTreeNode.child = new BTreeNode[2 * degree];
             bTreeNode.leafs = true;
-            bTreeNode.n = 0;
+            bTreeNode.height = 0;
             return bTreeNode;
         }
 
@@ -38,12 +46,12 @@ namespace DataStructure_And_Algorithm.Datastructure.tree
         public void InsertTheKey(int key)
         {
             BTreeNode tempRoot = rootNode;
-            if (tempRoot.n == 2 * degree - 1)
+            if (tempRoot.height == 2 * degree - 1)
             {
                 BTreeNode newNode = CreateNode();
                 rootNode = newNode;
                 newNode.leafs = false;
-                newNode.n = 0;
+                newNode.height = 0;
                 newNode.child[0] = tempRoot;
                 SplitNode(newNode, 0, tempRoot);
                 InsertTheNode(newNode, key);
@@ -60,7 +68,7 @@ namespace DataStructure_And_Algorithm.Datastructure.tree
         {
             BTreeNode tempNewNode = CreateNode();
             tempNewNode.leafs = tempRootNode.leafs;
-            tempNewNode.n = degree - 1;
+            tempNewNode.height = degree - 1;
 
             for (int j = 0; j < degree - 1; j++)
             {
@@ -75,22 +83,22 @@ namespace DataStructure_And_Algorithm.Datastructure.tree
                 }
             }
 
-            tempRootNode.n = degree - 1;
+            tempRootNode.height = degree - 1;
 
-            for (int j = newNode.n; j >= position + 1; j--)
+            for (int j = newNode.height; j >= position + 1; j--)
             {
                 newNode.child[j + 1] = newNode.child[j];
             }
 
             newNode.child[position + 1] = tempNewNode;
 
-            for (int j = newNode.n - 1; j >= position; j--)
+            for (int j = newNode.height - 1; j >= position; j--)
             {
                 newNode.keys[j + 1] = newNode.keys[j];
             }
 
             newNode.keys[position] = tempRootNode.keys[degree - 1];
-            newNode.n = newNode.n + 1;
+            newNode.height = newNode.height + 1;
         }
 
         //Insert the Node
@@ -99,24 +107,24 @@ namespace DataStructure_And_Algorithm.Datastructure.tree
             if (node.leafs)
             {
                 int i = 0;
-                for (i = node.n - 1; i >= 0 && key < node.keys[i]; i--)
+                for (i = node.height - 1; i >= 0 && key < node.keys[i]; i--)
                     node.keys[i + 1] = node.keys[i];
 
                 node.keys[i + 1] = key;
-                node.n = node.n + 1;
+                node.height = node.height + 1;
             }
 
             else
             {
                 int i = 0;
-                
-                for (i = node.n - 1; i >= 0 && key < node.keys[i]; i--) { }
-                
+
+                for (i = node.height - 1; i >= 0 && key < node.keys[i]; i--) { }
+
                 i += 1;
 
                 BTreeNode tempChildNode = node.child[i];
 
-                if (tempChildNode.n == (2 * degree - 1))
+                if (tempChildNode.height == (2 * degree - 1))
                 {
                     SplitNode(node, i, tempChildNode);
                     if (key > node.keys[i])
@@ -136,17 +144,102 @@ namespace DataStructure_And_Algorithm.Datastructure.tree
 
         private void DisplayNode(BTreeNode rootNode)
         {
-            for (int i = 0; i < rootNode.n; i++)
+            for (int i = 0; i < rootNode.height; i++)
                 Console.Write(rootNode.keys[i] + " ");
 
             if (!rootNode.leafs)
-                for (int i = 0; i < rootNode.n + 1; i++)
+                for (int i = 0; i < rootNode.height + 1; i++)
                     DisplayNode(rootNode.child[i]);
         }
 
+        //Search the key
+        private BTreeNode Search(BTreeNode root, int key)
+        {
+            int i = 0;
+            if (root == null)
+                return root;
+            for (i = 0; i < root.height; i++)
+            {
+                if (key < root.keys[i])
+                {
+                    break;
+                }
+                if (key == root.keys[i])
+                {
+                    return root;
+                }
+            }
+            if (root.leafs)
+            {
+                return null;
+            }
+            else
+            {
+                return Search(root.child[i], key);
+            }
+        }
+
+        private void RemoveKeys(int key)
+        {
+            BTreeNode foundNode = Search(this.rootNode, key);
+            if (foundNode == null)
+                return;
+
+        }
+
+
+        //private void RemoveKeys(BTreeNode rootNode, int key)
+        //{
+        //    int position = rootNode.FindKey(key);
+
+        //    if (position != -1)
+        //    {
+        //        if (rootNode.leafs)
+        //        {
+        //            int i = 0;
+        //            for (i = 0; i < rootNode.height && rootNode.keys[i] != key; i++) { }
+
+        //            for (; i < rootNode.height; i++)
+        //            {
+        //                if (i != (2 * degree - 1))
+        //                {
+        //                    rootNode.keys[i] = rootNode.keys[i + 1];
+        //                }
+        //            }
+
+        //            rootNode.height--;
+        //            return;
+        //        }
+
+        //        if (!rootNode.leafs)
+        //        {
+        //            BTreeNode preNode = rootNode.child[position];
+        //            int preNodeKey = 0;
+        //            if (preNode.height >= degree)
+        //            {
+        //                for (; ; )
+        //                {
+        //                    if (preNode.leafs)
+        //                    {
+        //                        Console.WriteLine(preNode.height);
+        //                        preNodeKey = preNode.keys[preNode.height - 1];
+        //                        break;
+        //                    }
+        //                    else
+        //                        preNode = preNode.child[preNode.height];
+        //                }
+
+        //                RemoveKeys(preNode, preNodeKey);
+        //                rootNode.keys[position] = preNodeKey;
+        //                return;
+        //            }
+        //        }
+        //    }
+        //}
+
         public void BTreeOperation_1()
         {
-            BTree b = new BTree(3);
+            BTree b = new BTree(2);
             //b.InitializeTheTree(3);
             b.InsertTheKey(8);
             b.InsertTheKey(9);
